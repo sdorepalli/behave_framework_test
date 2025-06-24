b149d2f35073adc163e92f271fec2f158d3f5049
sdorepalli <dorepallisatish@gmail.com>
2025-06-24 13:22:43 -0400

    4th commit

--- features/steps/steps_tutorials.py
+++ features/steps/steps_tutorials.py
@@ -28,6 +28,7 @@ def closeBrowser(context):
 #     status=context.find_element(By.XPATH,"//div[@class='orangehrm-login-branding']").is_displayed()
 #     assert status is True
 # Tests being passed
+# Tests are failed:
 
 
 
